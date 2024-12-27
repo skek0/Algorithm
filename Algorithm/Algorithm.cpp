d4ea873a07338abe0031214fe21e3e766c4852e3
skek0 <77053845+skek0@users.noreply.github.com>
2024-12-27 21:42:03 +0900

    2024.12.27 깊이우선탐색 2, 너비우선탐색

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,17 +1,16 @@
 ﻿#include <iostream>
 #include <vector>
-
+#include <queue>
 #define SIZE 8
 
 using namespace std;
 
-template<typename T>
 class Graph
 {
 private:
 	bool visited[SIZE];
 	vector<int> graph[SIZE];
-
+	queue<int> queue;
 public:
 	Graph()
 	{
@@ -20,50 +19,48 @@ public:
 			visited[i] = false;
 		}
 	}
-	void InsertEdge(int vertex, int edge)
+	void Insert(int vertex, int edge)
 	{
 		graph[vertex].push_back(edge);
 		graph[edge].push_back(vertex);
 	}
-	void Show()
+	void WFS(int start)
 	{
-		for (int i = 0; i < SIZE; i++)
+		queue.push(start);
+		cout << start << " ";
+		visited[start] = true;
+		for (int i = 0; i < graph[start].size(); i++)
 		{
-			cout << "graph[" << i << "] : ";
-			for (int j = 0; j < graph[i].size(); j++)
+			if (!visited[graph[start][i]])
 			{
-				cout << graph[i][j] << " ";
+				WFS(graph[start][i]);
 			}
-			cout << endl;
 		}
+		queue.pop();
 	}
 };
 
 int main()
 {
-#pragma region 깊이 우선 탐색 (Depth First Search)
-	// 루트 노드에서 시작해서 다음 분기로 넘어가기 전에
-	// 해당 분기를 완벽하게 탐색하는 방법
-	// 스택 자료구조 사용
+#pragma region 너비 우선 탐색
+	// 시작 정점을 방문한 후 시작 정점에 인접한 
+	// 모든 정점을 우선 방문하는 방법
 
-	// 1. 시작 노드를 스택에 넣고 방문처리
-	// 2. 스택의 최상단 노드에 방문하지 않은 인접 노드가 있으면
-	//	  그 노드를 스택에 넣고 방문 처리
-	// 3. 방문하지 않은 인접 노드가 없으면 스택에서 최상단 노드 꺼내기
-	// 4. 더 이상 2번의 과정을 수행 할 수 없을 때 까지 반복
+	// 더 이상 방문하지 않은 정점이 없을 때까지
+	// 방문하지 않은 모든 정점들에 대해서도 너비 우선 탐색을 적용
 
-	Graph<int> graph;
-	graph.InsertEdge(1, 2);
-	graph.InsertEdge(1, 3);
-	graph.InsertEdge(2, 4);
-	graph.InsertEdge(2, 5);
-	graph.InsertEdge(3, 6);
-	graph.InsertEdge(3, 7);
-	graph.InsertEdge(4, 5);
-	graph.InsertEdge(6, 7);
-	graph.Show();
+	Graph graph;
+
+	graph.Insert(1, 2);
+	graph.Insert(1, 3);
+	graph.Insert(2, 4);
+	graph.Insert(2, 5);
+	graph.Insert(3, 6);
+	graph.Insert(3, 7);
+
+	graph.WFS(1);
+
 #pragma endregion
-
 
 	return 0;
 }
---
+++ Class16th (Depth First Search)/Class16th (Depth First Search).cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <vector>
+
+#define SIZE 8
+
+using namespace std;
+
+template<typename T>
+class Graph
+{
+private:
+	bool visited[SIZE];
+	vector<int> graph[SIZE];
+
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			visited[i] = false;
+		}
+	}
+	void InsertEdge(int vertex, int edge)
+	{
+		graph[vertex].push_back(edge);
+		graph[edge].push_back(vertex);
+	}
+	void DFS(int start)
+	{
+		cout << start << " ";
+		visited[start] = true;
+		for (int i = 0; i < graph[start].size(); i++)
+		{
+			if (!visited[graph[start][i]])
+			{
+				DFS(graph[start][i]);
+			}
+		}
+	}
+
+	void Show()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			cout << "graph[" << i << "] : ";
+			for (int j = 0; j < graph[i].size(); j++)
+			{
+				cout << graph[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+};
+
+int main()
+{
+#pragma region 깊이 우선 탐색 (Depth First Search)
+	// 루트 노드에서 시작해서 다음 분기로 넘어가기 전에
+	// 해당 분기를 완벽하게 탐색하는 방법
+	// 스택 자료구조 사용
+
+	// 1. 시작 노드를 스택에 넣고 방문처리
+	// 2. 스택의 최상단 노드에 방문하지 않은 인접 노드가 있으면
+	//	  그 노드를 스택에 넣고 방문 처리
+	// 3. 방문하지 않은 인접 노드가 없으면 스택에서 최상단 노드 꺼내기
+	// 4. 더 이상 2번의 과정을 수행 할 수 없을 때 까지 반복
+
+	Graph<int> graph;
+	graph.InsertEdge(1, 2);
+	graph.InsertEdge(1, 3);
+	graph.InsertEdge(2, 3);
+	graph.InsertEdge(2, 4);
+	graph.InsertEdge(2, 5);
+	graph.InsertEdge(3, 6);
+	graph.InsertEdge(3, 7);
+	graph.InsertEdge(4, 5);
+	graph.InsertEdge(6, 7);
+
+	graph.DFS(1);
+#pragma endregion
+
+
+	return 0;
+}
